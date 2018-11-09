b63a8b2341fe4bba39acd45213855324077061ad
JacquieLeeYu <yu790@purdue.edu>
2018-11-09 16:20:36 -0500

    finished adding the file contents (No comments though)

--- src/ChatMessage.java
+++ src/ChatMessage.java
@@ -1,2 +1,9 @@
-public class ChatMessage {
+
+import java.io.Serializable;
+
+final class ChatMessage implements Serializable {
+    private static final long serialVersionUID = 6898543889087L;
+
+    // Here is where you should implement the chat message object.
+    // Variables, Constructors, Methods, etc.
 }
