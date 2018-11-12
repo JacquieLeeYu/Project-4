cf83783be9f8d548f1be8c65615f58d829234242
siddharthpillai <pillai8@purdue.edu>
2018-11-12 12:03:05 -0500

    Fixed *Added ChatFilter code to create a String Array of the words we need to filter.* Also added and tested filtering message functionality.

--- src/ChatFilter.java
+++ src/ChatFilter.java
@@ -11,36 +11,53 @@ import java.io.*;
  */
 
 public class ChatFilter {
+    private String badWordsFileName;
 
     public ChatFilter(String badWordsFileName) {
-        File file = new File(badWordsFileName);
-        try {FileReader fr = new FileReader(file);
-        BufferedReader br = new BufferedReader(fr);
-        String line = br.readLine();
-        String text = "";
-
-        while (line != null) {
-            text = text + line + "\n";
-            line = br.readLine();
-        }
-        String[] badWords = text.split("\n");
-
-
-
-        } catch (FileNotFoundException e) {
-            System.out.println("File Not Found");
-
-        } catch (IOException e) {
-            System.out.println("IOException");
-
-        }
-
+        this.badWordsFileName = badWordsFileName;
     }
-
     public String filter(String msg) {
+            File file = new File(badWordsFileName);
+            try {FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr);
+                String line = br.readLine();
+                String text = "";
+
+                while (line != null) {
+                    text = text + line + "\n";
+                    line = br.readLine();
+                }
+                String[] badWords = text.split("\n");
+                String[] filteredWords = new String[badWords.length];
+                for (int i = 0; i < badWords.length ; i++) {
+                    String word = "";
+                    for (int j = 0; j < badWords[i].length(); j++) {
+                        word += '*';
+                    }
+                    filteredWords[i] = word;
+                }
+
+                for (int i = 0; i < badWords.length ; i++) {
+                    msg = msg.replace(badWords[i],filteredWords[i]);
+                }
+                System.out.println(msg);
+
+
+
+            } catch (FileNotFoundException e) {
+                System.out.println("File Not Found");
+                return null;
+
+            } catch (IOException e) {
+                System.out.println("IOException");
+                return null;
+            }
+
+
         return msg;
     }
     public static void main(String[] args) {
-        new ChatFilter("/Users/siddharthpillai/Desktop/filterText.txt");
+        ChatFilter chat =  new ChatFilter("/Users/siddharthpillai/Desktop/filterText.txt");
+        chat.filter("I go to IU. It is a bad school");
     }
 }
